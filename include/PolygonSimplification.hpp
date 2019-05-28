13f26a7ef6bedfde6881de32234833ed4f29e78e
Thomas "elfprince13" Dickerson <elfprince13@gmail.com>
2019-05-28 17:15:15 -0400

    fixed formatting

--- include/PolygonSimplification.hpp
+++ include/PolygonSimplification.hpp
@@ -91,7 +91,7 @@ namespace com {
 				// Debug the current set of polys:
 				std::for_each(component_polys.cbegin(), component_polys.cend(), [](const PointIterPair &pip){
 						std::for_each(pip.first, pip.second, [](const CGALPoint &p){
-							std::cout << p;
+							std::cout << p << " ";
 						});
 						std::cout << std::endl;
 				});
